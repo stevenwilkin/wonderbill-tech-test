e77a68bceff93813b5a31a0432cd69d4dc5338c0
Steven Wilkin <stevenwilkin@gmail.com>
2017-01-06 17:32:58 +0000

    Save uploaded images and return an id

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
+require 'securerandom'
 
 class App < Sinatra::Base
-  get '/' do
+  helpers do
+    def image_dir
+      ENV.fetch('IMAGE_DIR', File.join(__dir__, 'images'))
+    end
+  end
+
+  post '/' do
+    image = params[:image]
+
+    halt 400 unless image
+    halt 400 unless image[:tempfile]
+
+    id = SecureRandom.uuid
+    extension = image[:filename].match(/.*\.(.*)$/)[1]
+    path = File.join(image_dir, "#{id}.#{extension}")
+    File.open(path, 'w') { |f| f.write image[:tempfile].read }
+
+    body id
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,11 +9,48 @@ RSpec.describe App do
     described_class.new
   end
 
-  describe 'GET /' do
-    before do
-      get '/'
+  let(:image_path) { File.join(__dir__, 'fixtures', 'logo.gif') }
+  let(:image) { Rack::Test::UploadedFile.new(image_path, 'image/gif') }
+  let(:uploads) { ENV['IMAGE_DIR'] }
+
+  before do
+    ENV['IMAGE_DIR'] = Dir.mktmpdir
+  end
+
+  after do
+    ENV.delete('IMAGE_DIR')
+  end
+
+  describe 'POST /' do
+    context 'when no request params are supplied' do
+      before do
+        post '/'
+      end
+
+      specify { expect(last_response.status).to eq(400)  }
     end
 
-    specify { expect(last_response).to be_ok  }
+    context 'when an image is not supplied' do
+      before do
+        post '/', image: 'foo'
+      end
+
+      specify { expect(last_response).to_not be_ok  }
+    end
+
+    context 'when an image is supplied' do
+      let(:returned_id) { last_response.body }
+      let(:uploaded_path) { File.join(uploads, "#{returned_id}.gif") }
+
+      before do
+        post '/', image: image
+      end
+
+      specify { expect(last_response).to be_ok }
+      specify { expect(File.exists?(uploaded_path)).to be_truthy }
+      specify do
+        expect(FileUtils.compare_file(uploaded_path, image_path)).to be_truthy
+      end
+    end
   end
 end
